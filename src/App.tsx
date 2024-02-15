60ba3ab6fb2f9aefdaea17971f4fb6c8156e3caf
Nemanja Risteski <risteski.nemanja5@gmail.com>
2024-02-15 15:08:57 -0500

    Fixed Bugs and removed banner

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,6 @@ import TasksEditPage from "./pages/tasks/edit";
 function App() {
   return (
     <BrowserRouter>
-      <GitHubBanner />
       <RefineKbarProvider>
         <AntdApp>
           <DevtoolsProvider>
