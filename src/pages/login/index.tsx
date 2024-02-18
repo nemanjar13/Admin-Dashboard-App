a4eb03aeeee5e8f6b703e76f3f647f41a973492c
Nemanja Risteski <risteski.nemanja5@gmail.com>
2024-02-18 16:17:22 -0500

    Fixed Bugs

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,15 +1,14 @@
-import {
-    AuthPage,
-} from "@refinedev/antd";
+import { AuthPage } from "@refinedev/antd";
 import { authCredentials } from "../../providers";
 
 export const Login = () => {
-    return (
-        <AuthPage
-            type="login"
-            formProps={{ 
-                initialValues: authCredentials
-            }}
-        />
-    );
+  return (
+    <AuthPage
+      type="login"
+      registerLink={false}
+      formProps={{
+        initialValues: authCredentials,
+      }}
+    />
+  );
 };
